8010970828e1e2e629807f7d76756e8fc38e6741
Einar Persson <me@einarpersson.com>
2020-09-11 10:47:57 +0200

    Hello world. Starting up

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,15 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.write("Starting up...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
